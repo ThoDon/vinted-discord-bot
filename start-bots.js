9eaebeeb082a3703d6286bf33ee829318ad304d5
Thomas <48889873+ThoDon@users.noreply.github.com>
2022-03-03 15:27:06 +0100

    Updated script to build local docker dependency easily

--- start-bots.js
+++ start-bots.js
@@ -1,63 +1,80 @@
-const exec = require('child_process').exec;
-const bots = require('./bots.json');
+const exec = require("child_process").exec;
+const bots = require("./bots.json");
 
 const recreate = () => {
+  console.log(`💎 Creating local Docker dependency...`);
 
-    console.log(`🐳 Checking existing volumes...`);
+  exec(
+    `docker build --pull --rm -f "Dockerfile" -t vinted-discord-bot:3.17 "."`,
+    (err, stdout, stderr) => {
+      if (!err) {
+        console.log(`👋 Local dependency created`);
+        console.log(`🐳 Checking existing volumes...`);
 
-    exec(`docker volume ls -q`, (err, stdout, stderr) => {
+        exec(`docker volume ls -q`, (err, stdout, stderr) => {
+          const volumes = stdout.split("\n");
 
-        const volumes = stdout.split('\n');
-
-        bots.forEach((bot) => {
+          bots.forEach((bot) => {
             console.log(`🐋 Starting ${bot.name}...`);
 
             const start = () => {
-                exec(`BOT=${bot.name} VINTED_BOT_ADMIN_IDS=${bot.adminIDs} VINTED_BOT_TOKEN=${bot.token} docker-compose -f docker-compose.yaml -p bot-${bot.name} up -d`, (err, stdout, stderr) => {
-                    if (err) {
-                        console.error(`🐋 ${bot.name} failed to start.`);
-                        console.error(err);
-                        return;
-                    }
-                    console.log(stderr);
-                });
-            }
-            
+              exec(
+                `BOT=${bot.name} VINTED_BOT_ADMIN_IDS=${bot.adminIDs} VINTED_BOT_TOKEN=${bot.token} docker-compose -f docker-compose.yaml -p bot-${bot.name} up -d`,
+                (err, stdout, stderr) => {
+                  if (err) {
+                    console.error(`🐋 ${bot.name} failed to start.`);
+                    console.error(err);
+                    return;
+                  }
+                  console.log(stderr);
+                }
+              );
+            };
+
             if (volumes.includes(`bot-${bot.name}`)) {
-                console.log(`📦 ${bot.name} database has been recovered!`);
-                start();
+              console.log(`📦 ${bot.name} database has been recovered!`);
+              start();
             } else {
-                exec(`docker volume create bot-${bot.name}`, (err, stdout, stderr) => {
-                    if (!err) {
-                        console.log(`📦 ${bot.name} database has been created!`);
-                        start();
-                    } else console.error(err);
-                });
+              exec(
+                `docker volume create bot-${bot.name}`,
+                (err, stdout, stderr) => {
+                  if (!err) {
+                    console.log(`📦 ${bot.name} database has been created!`);
+                    start();
+                  } else console.error(err);
+                }
+              );
             }
-            
+          });
         });
-
-    });
-
+      } else {
+        console.log(`👎 Error creating local dependecy`);
+        throw err;
+      }
+    }
+  );
 };
 
-const restart = process.argv.includes('-restart');
+const restart = process.argv.includes("-restart");
 
 if (restart) {
-    console.log('👋 Shutting down all bots...');
-    bots.forEach((bot) => {
-        exec(`docker-compose -p bot-${bot.name} stop`, (err, stdout, stderr) => {
+  console.log("👋 Shutting down all bots...");
+  bots.forEach((bot) => {
+    exec(`docker-compose -p bot-${bot.name} stop`, (err, stdout, stderr) => {
+      if (!err) {
+        exec(
+          `docker-compose -p bot-${bot.name} rm -f`,
+          (err, stdout, stderr) => {
             if (!err) {
-                exec(`docker-compose -p bot-${bot.name} rm -f`, (err, stdout, stderr) => {
-                    if (!err) {
-                        console.log(`👋 Bot ${bot.name} has been shut down and removed.`);
-                    } else {
-                        console.log(`👎 Failed to remove containers for bot ${bot.name}`);
-                    }
-                });
+              console.log(`👋 Bot ${bot.name} has been shut down and removed.`);
+            } else {
+              console.log(`👎 Failed to remove containers for bot ${bot.name}`);
             }
-        });
+          }
+        );
+      }
     });
+  });
 } else {
-    recreate();
+  recreate();
 }
